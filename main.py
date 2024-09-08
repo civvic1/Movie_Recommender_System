5b3048ec7642498fb178837836db80c69adc24fd
Victor Carracedo <66072300+civvic1@users.noreply.github.com>
2024-09-08 17:07:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from functools import lru_cache
 import numpy as np
 import pandas as pd
 
-import scikit
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.neighbors import NearestNeighbors
 import requests
 
 import zipfile
